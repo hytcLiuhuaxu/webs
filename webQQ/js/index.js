a4f64575efdef2587b8222ff6b5124537cb50c94
Liuhuxu <407498779@qq.com>
2014-08-18 17:00:41 +0800

    2014.8.18
    webQQ

--- webQQ/js/index.js
+++ webQQ/js/index.js
@@ -1,3 +1,39 @@
 $(function(){
 	//alert("ok");
+	$(".next").click(
+		function(){
+			//alert("OK");
+			$(".bgAllImage").css({"display":"none"});
+			imgnext();
+		}
+	);
+	$(".pre").click(
+		function(){
+			$(".bgAllImage").css({"display":"none"});
+			imgpre();
+		}
+	);
 });
+var img=1;
+var html='';
+
+function imgnext(){
+	img++;
+	if(img>4)
+	{
+		img=1;
+	}
+	html +=' <img class="bgAllImage"  src="css/image/'+img+'.jpg">';
+	$("#bgAllImage").append(html);
+	
+	//alert(img);	
+}
+function imgpre(){
+	img--;
+	if(img<1)
+	{
+		img=4;
+	}
+	html +=' <img class="bgAllImage"  src="css/image/'+img+'.jpg">';
+	$("#bgAllImage").append(html);
+}
