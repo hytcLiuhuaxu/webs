3dededa9bfd58e81d47d63b32021dff089f0c37e
Liuhuxu <407498779@qq.com>
2014-08-19 11:42:05 +0800

    2014.8.19

--- webQQ/js/index.js
+++ webQQ/js/index.js
@@ -1,5 +1,53 @@
 $(function(){
 	//alert("ok");
+	$(".list_group_title").click(
+		function(){
+			var x=$(this).parent().find(".list_group_body").attr("pd");
+			if(x==0)
+			{
+				//alert(x);
+				$(this).css(
+					"background-image","url(css/image/open_arrow_fire.png)"
+				);
+				$(this).parent().find(".list_group_body").css({
+					"display":"block"
+				});
+				$(this).parent().find(".list_group_body").attr("pd","1");
+			}
+			if(x==1)
+			{
+				$(this).css({
+					"background-image":"",
+				});
+				$(this).parent().find(".list_group_body").css({
+					"display":"none"
+				});
+				$(this).parent().find(".list_group_body").attr("pd","0");
+			}
+		}
+	);
+
+	$("#memberTab li").click(
+		function(){
+			$("#memberTab li").removeClass("active");
+			$(this).addClass("active");
+			var ex=$(this).attr("ex");
+			//alert(ex);
+			$(".tab_body li").removeClass("active");
+			$("#memberTabBody-"+ex+"").addClass("active");
+		}
+	);
+	$(".nav_tab_head li").click(
+		function(){
+			$(".nav_tab_head li").removeClass("selected");
+			$(this).addClass("selected");
+			var nth=$(this).attr("nth");
+			//alert(nth);
+			$(".panel").removeClass("selected");
+			$("#panel-"+nth+"").addClass("selected");
+		}
+	);
+
 	$(".next").click(
 		function(){
 			//alert("OK");
