28c79e0d6b10c937a2f69fcdc1478c87e5655485
Liuhuxu <407498779@qq.com>
2014-08-21 18:53:53 +0800

    2014.8.21

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 $(function(){
-	
+	//alert("Ok");
+	/*$(".ul1 li").hover(
+		function(){
+			$(this).find("img").stop().animate({"opacity":"1"},1000);
+			
+		},
+		function(){
+			$(this).find("img").stop().animate({"opacity":"0.5"},1000);
+		}
+	);*/
 });
